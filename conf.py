040a73c8e9368be98b4b19968c56d059afaece17
phiarchitect <phi@phiarchitect.com>
2022-05-03 06:22:47 -0700

    add PHOTON theme

--- conf.py
+++ conf.py
@@ -11,7 +11,7 @@
 import os
 import sys
 import ablog
-import alabaster
+import photonsphinx
 
 # -- General ABlog Options ----------------------------------------------------
 
@@ -193,7 +193,7 @@ extensions = [
     'sphinx.ext.graphviz',
     'sphinx.ext.mathjax',
     'sphinx.ext.todo',
-    'alabaster',
+    'photonsphinx',
     'ablog',
 ]
 
@@ -272,7 +272,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'photonsphinx'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -282,7 +282,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [alabaster.get_path()]
+html_theme_path = [photonsphinx.get_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -367,8 +367,9 @@ htmlhelp_basename = "phiARCHITECTdoc"
 
 
 html_theme_options = {
-    #  'logo': 'logo.png',
+    'logo': 'phi-headshot-sqr.jpg',
     'github_user': 'phiarchitect',
+    'github_button': True,
     'base_bg': 'black',
     'base_text': 'white',
     'font_family': "'Fira Sans Condensed', sans-serif",
