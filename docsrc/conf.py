62029dd1dbc54cec120287cde74d4ac0ddb2ccce
phiarchitect <phi@phiarchitect.com>
2023-11-15 09:52:03 -0800

    update docs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -6,29 +6,5 @@ org_name = 'phi ARCHITECT'
 repo = 'phiarchitect.com'
 repo_name = repo
 
-blog_title = f'{org_name}'
-project = f'{org_name}'
-version = ""  # The short X.Y version.
-release = ""  # The full version, including alpha/beta/rc tags.
-
-copyright = f'{year}, {org_name}'
-author = "phi ARCHITECT"
-
-# Base URL for the website, required for generating feeds.
-# e.g. blog_baseurl = "http://example.com/"
-blog_baseurl = f'https://{repo}'
-html_base_url = blog_baseurl
-html_baseurl = blog_baseurl
-
-blog_authors = {
-    "phi": ("phi ARCHITECT", None),
-}
-
-html_theme_options = {
-    'logo': 'phi-headshot-sqr.jpg',
-    'logo_name': True,
-    'github_user': org,
-    'github_repo': repo,
-    'github_button': True,
-}
+setup_globals(org, org_name, repo, repo_name)
 
